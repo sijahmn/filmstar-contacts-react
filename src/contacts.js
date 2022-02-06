9e7f14d532a957cc5da6f4dd787ce7dba4d8bf6a
sijahmn <msijahmn@gmail.com>
2022-02-07 02:43:36 +0530

    moviestars details

---
+++ src/contacts.js
@@ -0,0 +1,27 @@
+
+const contacts = [
+  {
+    name: "Rajani kanth",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Rajinikanth_at_the_Inauguration_of_MGR_Statue.jpg/220px-Rajinikanth_at_the_Inauguration_of_MGR_Statue.jpg",
+    phone: "+91 7428731210",
+    email: "mail@Rajinikanth.com"
+  },
+  {
+    name: "Mammoottty",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Mammootty2013SOP.jpg/230px-Mammootty2013SOP.jpg",
+    phone: "+91 484 4046122",
+    email: "mail@mammootty.com"
+  },
+  {
+    name: "Manju",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Manju_Warrier_in_KLF_2018.jpg/220px-Manju_Warrier_in_KLF_2018.jpg",
+    phone: "0471 2336011",
+    email: "mail@manjuwarrior.com"
+
+  }
+];
+
+export default contacts;
